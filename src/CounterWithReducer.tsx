f25be0b2dbb181657ce30cfaca0c8263aa97bb23
pam190331 <pam130901@gmail.com>
2022-01-01 15:09:12 +0900

    useReducer2 commit.

--- src/CounterWithReducer.tsx
+++ src/CounterWithReducer.tsx
@@ -1,13 +1,20 @@
 import React, { useReducer } from 'react';
 
-const initialState = { count: 0 };
+// 73. useReducerの利用例
 
-function reducer(state: any, action: any) {
+type StateType = { count: number };
+const initialState: StateType = { count: 0 };
+
+type ActionType = { type: 'increment' | 'decrement' | 'reset' };
+
+function reducer(state: StateType, action: ActionType): StateType | never {
   switch (action.type) {
     case 'increment':
       return { count: state.count + 1 };
     case 'decrement':
       return { count: state.count - 1 };
+    case 'reset':
+      return initialState;
     default:
       throw new Error();
   }
@@ -20,6 +27,7 @@ function CounterWithReducer() {
       Count: {state.count}
       <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
       <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </>
   );
 }
