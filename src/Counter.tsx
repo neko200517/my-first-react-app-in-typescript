98919a72de20aee293d616aee9c98e35a55f8ba6
pam190331 <pam130901@gmail.com>
2022-01-01 14:42:07 +0900

    useRef-basic commit.

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-// 70. useRefとuseEffectを使ってみよう
+// 71. useRefのより実践的な使い方とOptional ChaningとNon-Null Assertion Operatorの紹介
 
 const Counter: React.FC<{}> = () => {
   const initialValue: any = 0;
@@ -19,12 +19,29 @@ const Counter: React.FC<{}> = () => {
     renderTimes.current++;
   });
 
+  // Non-Null Assertion Operator
+  const ref = useRef<HTMLInputElement>(null!);
+
+  const focusRef = () => {
+    // 1. basic
+    // const current = ref.current;
+    // if (current != null) current.focus();
+
+    // 2. Optinal Chaning
+    // ref.current?.focus();
+
+    // 3. Non-Null Assertion Operator
+    ref.current.focus();
+  };
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type='text' />
+      <button onClick={focusRef}>Click Me!</button>
     </div>
   );
 };
