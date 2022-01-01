40ad63314c51d0107b8408406c2a2b1cd3ee0073
pam190331 <pam130901@gmail.com>
2022-01-01 14:13:42 +0900

    useReducer commit.

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-// 69. useStateと型制約
+// 71. useRefのより実践的な使い方とOptinal ChainingとNon-Null Assertion Operator
 
 const Counter: React.FC<{}> = () => {
   const initialValue: any = 0;
@@ -14,11 +14,37 @@ const Counter: React.FC<{}> = () => {
     setValue((prevState) => prevState - 1);
   };
 
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current++;
+  });
+
+  // Non-Null Assertion Operator
+  // null!にするとnullでないことを明示できる
+  const ref = useRef<HTMLInputElement>(null!);
+
+  const focusInput = () => {
+    // 1. Nullを否定した標準的な書き方
+    // const current = ref.current;
+    // if (current != null) current.focus();
+
+    // 2. Optional Chaning
+    // 上記はこのように書き換えられる(!null)
+    // ref.current?.focus();
+
+    // 3. Non-Null Assertion Operatorを使用することによりnullが否定され、シンプルに実装するこができる
+    ref.current.focus();
+  };
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This Componnet was re-redered {renderTimes.current} times!</div>
+
+      <input ref={ref} type='text' />
+      <button onClick={focusInput}>Click Me</button>
     </div>
   );
 };
